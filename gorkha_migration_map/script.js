c54518cf155e2d94f3cebd9d5a47dc3622cc0c98
Rinjula <rinjuwaiba@gmail.com>
2026-02-27 13:18:30 +0000

    Increase map zoom level from 5.5 to 6.0

--- gorkha_migration_map/script.js
+++ gorkha_migration_map/script.js
@@ -7,7 +7,7 @@ const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/outdoors-v12",
   center: [88.5, 26.8], //Darjeeling–Dooars region 
-  zoom: 5.5
+  zoom: 6.0
 });
 
 // Controls
